7f1126b006dfee717f1665e71de5296bad6a1f01
GuilhermeVBeira <gui_doggao@hotmail.com>
2018-07-13 15:52:13 -0300

    Add validation for django migrations file

---
+++ hulks/check_django_migrations_filename.py
@@ -0,0 +1,26 @@
+import re
+import sys
+
+from hulks.base import BaseHook
+
+
+class DjangoMigrationHook(BaseHook):
+    MIGRATIONS_FILES_PATTERN = r'^([0-9]{4})\_+([\w])+_([0-9]{8})+_([0-9]{4}.py)'
+    # exemple pattern 0002_auto_20180315_0043.py
+
+    def validate(self, filename, **options):
+        pattern = self.MIGRATIONS_FILES_PATTERN
+        if re.match(pattern, filename):
+            print(f'{filename}: invalid migrations filename')
+            return False
+
+        return True
+
+
+def main(args=None):
+    hook = DjangoMigrationHook()
+    sys.exit(hook.handle(args))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
