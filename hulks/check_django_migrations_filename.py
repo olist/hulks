481336f28eba55386e0b971608b9fa6a4c2594d7
GuilhermeVBeira <gui_doggao@hotmail.com>
2018-07-16 09:49:23 -0300

    typo correction

--- hulks/check_django_migrations_filename.py
+++ hulks/check_django_migrations_filename.py
@@ -6,13 +6,13 @@ from hulks.base import BaseHook
 
 class DjangoMigrationHook(BaseHook):
     MIGRATIONS_FILES_PATTERN = r'.*[0-9]{4}_\w+_[0-9]{8}_[0-9]{4}.py$'
-    # exemple pattern 0002_auto_20180315_0043.py
+    # pattern example 0002_auto_20180315_0043.py
 
     def validate(self, filename, **options):
 
         pattern = self.MIGRATIONS_FILES_PATTERN
         if re.match(pattern, filename):
-            print(f'{filename}: invalid migrations filename')
+            print(f'{filename}: invalid migration filename')
             return False
 
         return True
