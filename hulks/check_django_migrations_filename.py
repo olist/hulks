0853f7cff8fdf9128b638531dd93da69b8d7b540
GuilhermeVBeira <gui_doggao@hotmail.com>
2018-07-17 10:10:29 -0300

    add camel case validation

--- hulks/check_django_migrations_filename.py
+++ hulks/check_django_migrations_filename.py
@@ -1,19 +1,30 @@
 import re
 import sys
+from pathlib import Path
 
 from hulks.base import BaseHook
 
 
 class DjangoMigrationFilenameHook(BaseHook):
-    MIGRATIONS_FILES_PATTERN = r'.*[0-9]{4}_\w+_[0-9]{8}_[0-9]{4}.py$'
+    MIGRATIONS_DEFAULT_FILES_PATTERN = r'.*[0-9]{4}_\w+_[0-9]{8}_[0-9]{4}'
+    CAMEL_CASE_PATTERN = r'^[0-9]{4}_[A-Z][a-z0-9]+[A-z][a-z]+'
 
     def validate(self, filename, **options):
-        pattern = self.MIGRATIONS_FILES_PATTERN
-        if re.match(pattern, filename):
-            print(f'{filename}: invalid migration filename')
-            return False
+        file = Path(filename)
+        valid = True
+        pattern = self.MIGRATIONS_DEFAULT_FILES_PATTERN
+        camel_case_pattern = self.CAMEL_CASE_PATTERN
 
-        return True
+        if re.match(pattern, file.name):
+            msg = 'invalid migration filename, default django name detected'
+            print(f'{filename}: {msg}')
+            valid = False
+
+        if re.match(camel_case_pattern, file.name):
+            msg = 'invalid migration filename, camel case detected'
+            print(f'{filename}: {msg}')
+            valid = False
+        return valid
 
 
 def main(args=None):
