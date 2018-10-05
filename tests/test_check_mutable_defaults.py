77e6906485dd23a585a6f7fe8c03fbeafebce04b
George Y. Kussumoto <georgeyk.dev@gmail.com>
2018-10-05 16:10:40 -0300

    check-mutable-defaults: add support for coroutines (asyncio)

--- tests/test_check_mutable_defaults.py
+++ tests/test_check_mutable_defaults.py
@@ -27,6 +27,20 @@ def test_function_with_mutable_default(capsys, hook, content):
         assert '(foo)' in output
 
 
+@pytest.mark.parametrize('content', [
+    'async def foo(var=[]): pass',
+    '@asyncio.coroutine\ndef foo(var={}): pass',
+    '@types.coroutine\ndef foo(var=set()): pass',
+])
+def test_async_function_with_mutable_default(capsys, hook, content):
+    with patch('builtins.open', mock_open(read_data=content)) as mock_file:
+        assert hook.validate('foo.py') is False
+        mock_file.assert_called_once_with('foo.py')
+
+        output, _ = capsys.readouterr()
+        assert '(foo)' in output
+
+
 @pytest.mark.parametrize('mutable_arg', ['[]', '{}', 'set()', 'list()', 'dict()', 'ValueError()'])
 def test_method_with_mutable_default(capsys, hook, mutable_arg):
     content = """
@@ -58,11 +72,28 @@ def test_dunder_method_with_mutable_default(capsys, hook):
         assert '(__init__)' in output
 
 
-def test_immutable_default(capsys, hook):
+@pytest.mark.parametrize('mutable_arg', ['[]', '{}', 'set()', 'list()', 'dict()', 'ValueError()'])
+def test_async_method_with_mutable_default(capsys, hook, mutable_arg):
     content = """
-    \ndef foo(arg0='', arg1=1, arg2=None, arg3=tuple(), arg4=object()):
+    \nclass A:
+        async def foo(self, arg={}):
+            pass
+    """.format(mutable_arg)
+
+    with patch('builtins.open', mock_open(read_data=content)) as mock_file:
+        assert hook.validate('foo.py') is False
+        mock_file.assert_called_once_with('foo.py')
+
+        output, _ = capsys.readouterr()
+        assert '(foo)' in output
+
+
+@pytest.mark.parametrize('prefix', ['', 'async ', '@asyncio.coroutine\n'])
+def test_immutable_default(capsys, hook, prefix):
+    content = """
+    \n{}def foo(arg0='', arg1=1, arg2=None, arg3=tuple(), arg4=object()):
         pass
-    """
+    """.format(prefix)
 
     with patch('builtins.open', mock_open(read_data=content)) as mock_file:
         assert hook.validate('foo.py') is True
